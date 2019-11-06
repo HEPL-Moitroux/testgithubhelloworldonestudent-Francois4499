5d18eb0386ae7ec042b90ecea1747b1a1151df7a
Francois4499 <57407928+Francois4499@users.noreply.github.com>
2019-11-06 19:35:59 +0100

    Add files via upload

---
+++ HelloWorldFrancoisSanglier-2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world\n");
+            Console.WriteLine("V01 06/11/2019 modifié par François\n");
+            Console.WriteLine("Cliqué sur une touche pour quitter\n");
+            Console.ReadKey();
+        }
+    }
+}
